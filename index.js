d2873af277785d31dc81a7030ae427a74bb89443
LAPTOP-S7Q8KMSK\ip323 <ip323@naver.com>
2021-12-09 23:33:23 +0900

    BodyParser & PostMan & 회원 가입 기능 & nodemon 설치

--- index.js
+++ index.js
@@ -2,14 +2,37 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const {User} = require('./models/User');
+const bodyParser = require('body-parser');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://youngrae:youngrae@boilerplate.mmvbn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-    useNewUrlParser: true, useUnifiedTopology: true
-}).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
+mongoose.connect('mongodb+srv://youngrae:youngrae@boilerplate.mmvbn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    // 회원 가입시 필요한 정보를 클라이언트에서 가져오면 서버에서 데이터베이스에 저장
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if (err) {
+            return res.json({success: false, err});
+        }
+        return res.status(200).json({
+            success: true,
+        })
+    });
+
 })
 
 app.listen(port, () => {
