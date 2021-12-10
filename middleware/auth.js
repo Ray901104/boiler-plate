ec131c2432fde43f4ec32816ba365c19a34cbab1
LAPTOP-S7Q8KMSK\ip323 <ip323@naver.com>
2021-12-10 21:35:39 +0900

    비밀번호 암호화 / 로그인 / 토큰 / Auth / 로그아웃

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const {User} = require('../models/User');
+
+let auth = (req,res,next) => {
+    // 인증 처리
+
+    // 클라이언트 쿠키에서 토큰 가져오기
+    let token = req.cookies.x_auth;
+
+    // 토큰을 복호화한 뒤 유저 찾기
+    User.findByToken(token, (err, user)=> {
+        if(err) throw err;
+        // 유저가 없으면 인증 NO
+        if(!user) return res.json({isAuth: false, error: true})
+
+        // 유저가 있으면 인증 OK
+        req.token = token;
+        req.user = user;
+        next();
+    })
+
+
+
+}
+
+module.exports = {auth};
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +33,62 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function (next) {
+    const user = this;
+
+    if(user.isModified('password')){
+        // 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if(err) return next(err);
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if(err) return next(err);
+
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+})
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    // plainPassword: 1234567 (암호화된 비밀번호) $2b$10$OSZZoqwViZG/ZhU6sO8xAOO4Mw0.0ULCc11.GjV/NO/MK4PEJBGGK 비교
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err);
+
+        cb(null, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    const user = this;
+
+    //jsonwebtoken 이용해서 토큰 생성
+    const token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+    user.token = token;
+    user.save(function (err, userInfo) {
+        if(err) return cb(err);
+        cb(null, userInfo);
+    });
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    const user = this;
+
+    //토큰 복호화
+    jwt.verify(token, 'secretToken', function(err, decoded){
+        // 유저 아이디를 이용해서 유저 찾은 뒤
+        // 클라이언트에서 가져온 token과 DB에 보관된 token의 일치 여부 확인
+        user.findOne({"_id":decoded, "token": token}, function(err, user){
+            if(err) return cb(err);
+            cb(null, user);
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema);
 
