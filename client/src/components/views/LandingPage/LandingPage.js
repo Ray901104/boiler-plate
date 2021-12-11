9c732f14a7baaa7c9a0323de19088c06c0a12b90
LAPTOP-S7Q8KMSK\ip323 <ip323@naver.com>
2021-12-11 17:07:01 +0900

    회원가입, 로그아웃

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
   useEffect(() => {
@@ -7,6 +8,19 @@ function LandingPage() {
       console.log(response.data);
     });
   });
+
+  const navigate = useNavigate();
+
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      console.log(response.data);
+      if (response.data.success) {
+        navigate("/login");
+      } else {
+        alert("로그아웃 실패");
+      }
+    });
+  };
 
   return (
     <div
@@ -19,6 +33,7 @@ function LandingPage() {
       }}
     >
       <h2>시작 페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
